5642eef15b688696fb8a3a94de330544a5d06236
drexelramos <drexelramos@gmail.com>
2020-10-21 06:07:27 -0700

    initial commit

---
+++ src/countrySelector.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './countrySelector.css';
+
+class CountrySelector extends Component {
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default CountrySelector;
--- src/demonym.test.js
+++ src/demonym.test.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import App from './App';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import Demonym from './demonym.js';
 
-
-describe('App Component', () => {
+describe('Demonym Component', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<App />, div);
+        ReactDOM.render(<Demonym />, div);
         ReactDOM.unmountComponentAtNode(div);
     })
 })
